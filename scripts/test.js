f56db5181d484534ae6b0ff209b8fd5492a00497
Nikolay Bychinskiy <2071936+DemonikGH@users.noreply.github.com>
2022-08-09 22:35:58 +0500

    Delete test.js
    Signed-off-by: Nikolay Bychinskiy <2071936+DemonikGH@users.noreply.github.com>

--- scripts/test.js
+++
@@ -1,9 +0,0 @@
-const mod = {
-    async load() {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        console.log('mod loaded');
-    }
-}
-addEventListener('gameload', e => {
-    e.game.registerMod('mod', mod);
-});
